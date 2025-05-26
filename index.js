878432cc93ef9274ee234976c366a862305f2bd4
Hamza <hamza.zulqarnain@perpetualpress.io>
2025-05-26 23:17:46 +0500

    make dyno small

--- index.js
+++ index.js
@@ -471,7 +471,7 @@ const getDealsHtml2 = (products, settings, tagStyles = {}) => {
               </td>
               <td style="vertical-align: middle;">
                 <a href="https://datadyno.co/user/deals" target="_blank" style="display: inline-block; text-decoration: none;">
-                  <img src="https://res.cloudinary.com/dh5pf5on1/image/upload/v1747049158/temp/hff1b0ossms0dvgofjkz.png" alt="Brand Logo" style="width: 130px; height: 35px; display: block; border: 0;" />
+                  <img src="https://res.cloudinary.com/dh5pf5on1/image/upload/v1747049158/temp/hff1b0ossms0dvgofjkz.png" alt="Brand Logo" style="width: 100px; height: 30px; display: block; border: 0;" />
                 </a>
               </td>
             </tr>
